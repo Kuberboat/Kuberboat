342950ca13ac34071bb3d70ada660676582505c9
xx01cyx <caoyuanxin0531@outlook.com>
2022-05-03 12:38:47 +0000

    !60 chore(test): modify test all script for ci error detection

--- test/kuberboat/test_all.sh
+++ test/kuberboat/test_all.sh
@@ -2,7 +2,26 @@
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+declare -i exit_code
+exit_code=0
+
 bash $parent_path/test_node.sh
+if [ $? -ne 0 ]
+    then exit_code=1
+fi
 bash $parent_path/test_pod.sh
+if [ $? -ne 0 ]
+    then exit_code=1
+fi
+
 bash $parent_path/test_service.sh
+if [ $? -ne 0 ]
+    then exit_code=1
+fi
+
 bash $parent_path/test_deployment.sh
+if [ $? -ne 0 ]
+    then exit_code=1
+fi
+
+exit $exit_code
